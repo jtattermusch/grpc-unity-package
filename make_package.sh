d8fe6c2b67db19c418dac817a54e0cf2d81b51fd
Jan Tattermusch <jtattermusch@google.com>
2018-07-31 13:38:27 +0200

    improve make_package script

--- make_package.sh
+++ make_package.sh
@@ -11,7 +11,7 @@ cd -
 
 # versions need to match with packages/packages.config
 PROTOBUF_PKG=Google.Protobuf.3.6.0
-GRPC_CORE_PKG=Grpc.Core.1.13.1
+GRPC_CORE_PKG=Grpc.Core.1.15.0-dev
 INTERACTIVE_ASYNC_PKG=System.Interactive.Async.3.1.1
 
 cp packages/${PROTOBUF_PKG}/lib/net45/Google.Protobuf.dll                         Plugins/Google.Protobuf/lib/net45/Google.Protobuf.dll
@@ -28,23 +28,18 @@ cp packages/${GRPC_CORE_PKG}/runtimes/win/native/grpc_csharp_ext.x64.dll        
 cp packages/${INTERACTIVE_ASYNC_PKG}/lib/net45/System.Interactive.Async.dll       Plugins/System.Interactive.Async/lib/net45/System.Interactive.Async.dll
 cp packages/${INTERACTIVE_ASYNC_PKG}/lib/net45/System.Interactive.Async.xml       Plugins/System.Interactive.Async/lib/net45/System.Interactive.Async.xml
 
-# TODO: add iOS library
-# TODO: add Android libraries
+# add Android and iOS native libraries
+cp packages/${GRPC_CORE_PKG}/runtimes/monoandroid/armeabi-v7a/libgrpc_csharp_ext.so  Plugins/Grpc.Core/runtimes/android/armeabi-v7a/libgrpc_csharp_ext.so
+cp packages/${GRPC_CORE_PKG}/runtimes/monoandroid/arm64-v8a/libgrpc_csharp_ext.so    Plugins/Grpc.Core/runtimes/android/arm64-v8a/libgrpc_csharp_ext.so
+cp packages/${GRPC_CORE_PKG}/runtimes/monoandroid/x86/libgrpc_csharp_ext.so          Plugins/Grpc.Core/runtimes/android/x86/libgrpc_csharp_ext.so
+cp packages/${GRPC_CORE_PKG}/runtimes/ios/native/libgrpc_csharp_ext.a                Plugins/Grpc.Core/runtimes/ios/libgrpc_csharp_ext.a
+cp packages/${GRPC_CORE_PKG}/runtimes/ios/native/libgrpc.a                           Plugins/Grpc.Core/runtimes/ios/libgrpc.a
 
 # TODO: add Grpc.Auth package..
 # TODO: add Grpc.Tools package...
 
-# Copy Custom Plugins Files
+# TODO: add file with package versions...
 
-#cp build.android/libgrpc_csharp_ext.so                Plugins/Grpc.Core/runtimes/android/armv7/libgrpc_csharp_ext.so
-#cp build.ios/libaddress_sorting.a                     Plugins/Grpc.Core/runtimes/ios/libaddress_sorting.a
-#cp build.ios/third_party/cares/cares/lib/libcares.a   Plugins/Grpc.Core/runtimes/ios/libcares.a
-#cp build.ios/third_party/boringssl/crypto/libcrypto.a Plugins/Grpc.Core/runtimes/ios/libcrypto.a
-#cp build.ios/libgpr.a                                 Plugins/Grpc.Core/runtimes/ios/libgpr.a
-#cp build.ios/libgrpc.a                                Plugins/Grpc.Core/runtimes/ios/libgrpc.a
-#cp build.ios/libgrpc_csharp_ext.a                     Plugins/Grpc.Core/runtimes/ios/libgrpc_csharp_ext.a
-#cp build.ios/third_party/boringssl/ssl/libssl.a       Plugins/Grpc.Core/runtimes/ios/libssl.a
-#cp build.ios/third_party/zlib/libz.a                  Plugins/Grpc.Core/runtimes/ios/libz.a
 
 # Zipping
 
